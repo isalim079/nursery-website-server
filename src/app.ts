14e991ee2d329101c01c8012129e1cbb5e530bd1
isalim079 <salimreza1025@gmail.com>
2024-07-13 20:08:32 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running correctly..!');
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
+
+export default app;
